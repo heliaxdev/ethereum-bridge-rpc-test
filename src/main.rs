4b0a032b970d19a70e81944be9d91d7e0788d554
Tiago Carvalho <sugoiuguu@tfwno.gf>
2023-01-25 16:36:45 +0000

    Relay validator set update

--- src/main.rs
+++ src/main.rs
@@ -7,55 +7,91 @@ use std::sync::Arc;
 
 use ethers::core::types::Address;
 use ethers::providers::{Http, Provider};
+use ethers::types::{H160, U256};
 use eyre::WrapErr;
 
-use self::contracts::{bridge, test_erc20};
-use self::namada_queries::ExecuteQuery;
+use self::contracts::governance::{Governance, Signature, ValidatorSetArgs};
+use self::namada_queries::{ExecuteQuery, QueryExecutor};
+
+/// Arguments to the validator set update relay call.
+struct RelayArgs {
+    /// The set of active validators.
+    validators: Vec<H160>,
+    /// The voting powers of the set of active validators.
+    voting_powers: Vec<U256>,
+    /// The current epoch.
+    current_epoch: U256,
+    /// A hash of the next set of validators' hot keys.
+    next_bridge_validator_set_hash: [u8; 32],
+    /// A hash of the next set of validators' cold keys.
+    next_governance_validator_set_hash: [u8; 32],
+    /// The signatures over the next set of validators.
+    proof: Vec<Signature>,
+}
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
-    let query = namada_queries::QueryExecutor::validator_set_update_proof();
-    let response = query.execute_query()?;
-    println!("{response:#?}");
+    println!("Fetching active validator set...");
+
+    let query = QueryExecutor::active_validator_set();
+    let valset_args = query.execute_query()?;
+    let epoch = valset_args.nonce.as_u64();
+
+    println!("Done! The current epoch is {epoch}.");
+    println!("Now fetching a validator set update proof...");
+
+    let query = QueryExecutor::validator_set_update_proof().at_epoch(epoch + 1);
+    let (bridge_hash, gov_hash, proof) = query.execute_query()?;
+
+    println!("Done! Relaying proof...");
+
+    // Connect to the network. We are assuming that a `hardhat` node is running in the
+    // background, with some contracts already deployed.
+    //
+    // <https://github.com/sug0/ethereum-bridge/tree/tiago/test/deploy-contracts>
+    let client = Arc::new(
+        Provider::<Http>::try_from("http://localhost:8545").wrap_err("Failed to get provider")?,
+    );
+    let args = RelayArgs {
+        validators: valset_args.validators,
+        voting_powers: valset_args.powers,
+        current_epoch: valset_args.nonce,
+        next_bridge_validator_set_hash: bridge_hash,
+        next_governance_validator_set_hash: gov_hash,
+        proof,
+    };
+    relay_proof(Arc::clone(&client), args).await?;
+
+    println!("Validator set update successfully relayed!");
     Ok(())
-    /*
-        // Connect to the network. We are assuming that a `hardhat` node is running in the
-        // background, with some contracts already deployed.
-        //
-        // <https://github.com/sug0/ethereum-bridge/tree/tiago/test/deploy-contracts>
-        let client = Arc::new(
-            Provider::<Http>::try_from("http://localhost:8545").wrap_err("Failed to get provider")?,
-        );
-
-        test_abigen_transfer_eth_to_nam(Arc::clone(&client)).await?;
-
-        Ok(())
-    */
 }
 
-/// Perform a transfor of some TestERC20 tokens to an arbitrary Namada address.
-async fn test_abigen_transfer_eth_to_nam(client: Arc<Provider<Http>>) -> eyre::Result<()> {
-    let bridge_address = "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707".parse::<Address>()?;
-    let test_erc20_address = "0x5FbDB2315678afecb367f032d93F642f64180aa3".parse::<Address>()?;
+/// Relay a validator set update to Ethereum.
+async fn relay_proof(client: Arc<Provider<Http>>, args: RelayArgs) -> eyre::Result<()> {
+    let RelayArgs {
+        validators,
+        voting_powers,
+        current_epoch,
+        next_bridge_validator_set_hash,
+        next_governance_validator_set_hash,
+        proof,
+    } = args;
 
-    let bridge = bridge::Bridge::new(bridge_address, Arc::clone(&client));
-    let test_erc20 = test_erc20::TestERC20::new(test_erc20_address, client);
+    let governance_address = "0x0165878A594ca255338adfa4d48449f69242Eb8F".parse::<Address>()?;
+    let governance = Governance::new(governance_address, client);
 
-    let approve_result = test_erc20.approve(bridge_address, 100.into()).await?;
-
-    if !approve_result {
-        return Err(eyre::eyre!("TestERC20 transfer tx not approved"));
-    }
-
-    let transfers = vec![bridge::NamadaTransfer {
-        from: test_erc20_address,
-        to: "atest1v4ehgw36xuunwd6989prwdfkxqmnvsfjxs6nvv6xxucrs3f3xcmns3fcxdzrvvz9xverzvzr56le8f"
-            .into(),
-        amount: 100.into(),
-    }];
-
-    let transf_result_call = bridge.transfer_to_namada(transfers, 1.into());
-    let pending_tx = transf_result_call.send().await?;
+    let relay_op = governance.update_validators_set(
+        ValidatorSetArgs {
+            validators,
+            powers: voting_powers,
+            nonce: current_epoch,
+        },
+        next_bridge_validator_set_hash,
+        next_governance_validator_set_hash,
+        proof,
+        current_epoch + 1,
+    );
+    let pending_tx = relay_op.send().await?;
 
     // The method `.send()` is used for mutable calls. For this reason, it
     // needs a certain number of block confirmations.

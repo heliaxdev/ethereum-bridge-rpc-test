20e9006df87f0fc8a2771de29cd0f7316910139a
Tiago Carvalho <sugoiuguu@tfwno.gf>
2023-01-27 15:33:23 +0000

    Query nonce and valset

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 use std::convert::TryFrom;
-use std::fs;
+
 use std::sync::Arc;
 
-use ethers::abi::FixedBytes;
-use ethers::contract::Contract;
-use ethers::core::{abi::Abi, types::Address};
+use ethers::core::types::Address;
 use ethers::providers::{Http, Provider};
 use eyre::WrapErr;
 
@@ -42,37 +40,8 @@ async fn main() -> eyre::Result<()> {
     );
 
     test_abigen_current_val_set(Arc::clone(&client)).await?;
-    test_abigen_transfer_eth_to_nam(Arc::clone(&client)).await?;
-    test_runtime_current_val_set(
-        Arc::try_unwrap(client).map_err(|_| eyre::eyre!("Failed to unwrap client Arc"))?,
-    )
-    .await?;
+    test_abigen_current_nonce(Arc::clone(&client)).await?;
 
-    Ok(())
-}
-
-/// Read the current validator set. The contract is initialized from a JSON file
-/// read at runtime.
-async fn test_runtime_current_val_set(client: Provider<Http>) -> eyre::Result<()> {
-    let bridge_address = "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707".parse::<Address>()?;
-    let bridge_abi: Abi = serde_json::from_str(&{
-        let path = "res/Bridge.abi";
-        fs::read_to_string(path).wrap_err_with(|| format!("Failed to read file path: {path}"))?
-    })
-    .wrap_err("Failed to parse json")?;
-
-    // Instantiate a contract at runtime, from the provided
-    // parsed ABI.
-    let contract = Contract::new(bridge_address, bridge_abi, client);
-
-    // Call the contract. We do the type checking manually.
-    let valset = contract
-        .method::<_, FixedBytes>("currentValidatorSetHash", ())?
-        .call()
-        .await
-        .wrap_err("Failed to get currentValidatorSetHash")?;
-
-    println!("{valset:?}");
     Ok(())
 }
 
@@ -88,34 +57,13 @@ async fn test_abigen_current_val_set(client: Arc<Provider<Http>>) -> eyre::Resul
     Ok(())
 }
 
-/// Perform a transfor of some TestERC20 tokens to an arbitrary Namada address.
-async fn test_abigen_transfer_eth_to_nam(client: Arc<Provider<Http>>) -> eyre::Result<()> {
-    let bridge_address = "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707".parse::<Address>()?;
-    let test_erc20_address = "0x5FbDB2315678afecb367f032d93F642f64180aa3".parse::<Address>()?;
-
-    let bridge = bridge::Bridge::new(bridge_address.clone(), Arc::clone(&client));
-    let test_erc20 = test_erc_20::TestERC20::new(test_erc20_address.clone(), client);
-
-    let approve_result = test_erc20.approve(bridge_address, 100.into()).await?;
-
-    if !approve_result {
-        return Err(eyre::eyre!("TestERC20 transfer tx not approved"));
-    }
-
-    let transfers = vec![bridge::NamadaTransfer {
-        from: test_erc20_address,
-        to: "atest1v4ehgw36xuunwd6989prwdfkxqmnvsfjxs6nvv6xxucrs3f3xcmns3fcxdzrvvz9xverzvzr56le8f"
-            .into(),
-        amount: 100.into(),
-    }];
-
-    let transf_result_call = bridge.transfer_to_namada(transfers, 1.into());
-    let pending_tx = transf_result_call.send().await?;
-
-    // The method `.send()` is used for mutable calls. For this reason, it
-    // needs a certain number of block confirmations.
-    let transf_result = pending_tx.confirmations(3).await?;
-    println!("{transf_result:?}");
-
+/// Read the current nonce.
+async fn test_abigen_current_nonce(client: Arc<Provider<Http>>) -> eyre::Result<()> {
+    let governance_address = "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9".parse::<Address>()?;
+    let governance = governance::Governance::new(governance_address, client);
+    // The method `.call()` is used for read-only calls. Therefore, it does
+    // not need to be sent as a tx, and does not need block confirmations.
+    let nonce = governance.validator_set_nonce().call().await?;
+    println!("{nonce}");
     Ok(())
 }
